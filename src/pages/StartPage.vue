225a743095de7c4fb676f12783ff9f5dd817d958
konstapascal <pascalkonstantinos@gmail.com>
2022-02-19 15:02:03 +0100

    CHORE: Small changes

--- src/pages/StartPage.vue
+++ src/pages/StartPage.vue
@@ -1,16 +1,13 @@
 <script setup lang="ts">
+import { ref } from 'vue';
 import router from '../router';
 import { useStore } from '../store';
 
 const store = useStore();
+const username = ref('');
 
-function onButtonClicked() {
-	const inputElement: HTMLInputElement | null = document.querySelector('#username');
-
-	// Setting username in state, redirecting user to questions
-	store.commit('setUsername', inputElement?.value);
-	router.push('/questions');
-}
+const onUsernameChange = () => store.commit('setUsername', username.value);
+const onButtonClick = () => router.push('/questions');
 </script>
 
 <template>
@@ -18,13 +15,15 @@ function onButtonClicked() {
 		<h1 class="text-4xl">Start Page</h1>
 		<label for="username">Choose your username</label>
 		<input
+			v-model="username"
+			@input="onUsernameChange()"
 			class="border-2 block px-2 py-1 mt-4 text-gray-900 rounded-sm"
 			type="text"
 			name="username"
 			id="username"
 		/>
 		<button
-			@click="onButtonClicked()"
+			@click="onButtonClick()"
 			class="bg-blue-600 hover:bg-blue-700 active:bg-blue-800 rounded-sm transition-colors py-2 px-4 mt-4"
 		>
 			Choose name
--- src/router.ts
+++ src/router.ts
@@ -1,25 +1,23 @@
-import { createWebHistory, createRouter } from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router';
 
-import StartPage from './pages/StartPage.vue';
 import QuestionsPage from './pages/QuestionsPage.vue';
 import ResultsPage from './pages/ResultsPage.vue';
-
-const routes = [
-	{
-		path: '/',
-		component: StartPage
-	},
-	{
-		path: '/questions',
-		component: QuestionsPage
-	},
-	{
-		path: '/results',
-		component: ResultsPage
-	}
-];
+import StartPage from './pages/StartPage.vue';
 
 export default createRouter({
 	history: createWebHistory(),
-	routes
+	routes: [
+		{
+			path: '/',
+			component: StartPage
+		},
+		{
+			path: '/questions',
+			component: QuestionsPage
+		},
+		{
+			path: '/results',
+			component: ResultsPage
+		}
+	]
 });
