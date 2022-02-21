6e9da1176ef2ff5637dfecda3fdb9ab39618601d
konstapascal <pascalkonstantinos@gmail.com>
2022-02-21 14:54:35 +0100

    FEAT: Styled questions page

--- src/pages/NotFoundPage.vue
+++ src/pages/NotFoundPage.vue
@@ -2,13 +2,15 @@
 import { onMounted } from 'vue';
 import router from '../router';
 
-onMounted(async () => setTimeout(() => router.push('/'), 2000));
+onMounted(async () => setTimeout(() => router.push('/'), 10000));
 </script>
 
 <template>
-	<section>
-		<h1 class="text-4xl">Not Found page</h1>
-		<p>You might be lost</p>
-		<p class="text-lg mt-4">Redirecting you...</p>
+	<section class="container mx-auto flex flex-col items-center">
+		<h1 class="text-4xl">Not Found Page</h1>
+		<div class="mt-4">
+			<p>You might be lost</p>
+			<p class="text-lg mt-4">Redirecting you...</p>
+		</div>
 	</section>
 </template>
--- src/pages/QuestionsPage.vue
+++ src/pages/QuestionsPage.vue
@@ -33,27 +33,38 @@ function onSubmitQuestionsClick() {
 </script>
 
 <template>
-	<section class="container mx-auto flex flex-col items-center">
+	<section class="container mx-auto my-14 flex flex-col items-center">
 		<h1 class="text-4xl">Questions Page</h1>
 		<div class="max-w-md">
-			<div class="mt-6" v-for="({ question, answers }, idx) in questions" :key="question">
+			<div
+				class="mt-8 text-center"
+				v-for="({ question, answers }, idx) in questions"
+				:key="question"
+			>
 				<h2 class="text-lg font-semibold">{{ question }}</h2>
-				<div v-for="answer in answers">
-					<input
-						type="radio"
-						:id="answer"
-						:value="answer"
-						v-model="pickedAnswers[idx]"
-						@change="onAnswerChange()"
-					/>
-					<label :for="answer">{{ answer }}</label>
+				<div class="mt-6">
+					<div class="" v-for="answer in answers">
+						<input
+							class="peer hidden"
+							type="radio"
+							:id="answer"
+							:value="answer"
+							v-model="pickedAnswers[idx]"
+							@change="onAnswerChange()"
+						/>
+						<label
+							class="block bg-gray-700 mt-2 py-2 peer-checked:bg-yellow-600 hover:cursor-pointer transition-colors"
+							:for="answer"
+							>{{ answer }}</label
+						>
+					</div>
 				</div>
 			</div>
-			<div class="flex justify-center">
+			<div class="flex justify-center mt-12">
 				<button
 					v-if="allAnswersPicked"
 					@click="onSubmitQuestionsClick()"
-					class="bg-blue-600 hover:bg-blue-700 active:bg-blue-800 rounded-sm transition-colors py-2 px-4 mt-4"
+					class="bg-blue-600 hover:bg-blue-700 active:bg-blue-800 rounded-sm transition-colors py-2 px-4"
 				>
 					Submit
 				</button>
